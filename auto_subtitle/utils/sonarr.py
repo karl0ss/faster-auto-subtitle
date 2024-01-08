69a555a658dc2686d9976279cda2db04e35b8fd0
Karl <karl@k-world.me.uk>
2024-01-08 09:22:21 +0000

    sonarr token from envavr

--- auto_subtitle/utils/sonarr.py
+++ auto_subtitle/utils/sonarr.py
@@ -1,6 +1,7 @@
 import requests
 import json
-
+import os
+token = os.getenv('sonarr_token')
 
 def update_show_in_soarr(show_id):
     url = "http://192.168.4.9:8989/api/v3/command"
@@ -11,7 +12,7 @@ def update_show_in_soarr(show_id):
     })
     headers = {
   'Content-Type': 'application/json',
-  'X-Api-Key': 'f6ea49a75a44469daec03969bdf6764d',
+  'X-Api-Key': token,
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
